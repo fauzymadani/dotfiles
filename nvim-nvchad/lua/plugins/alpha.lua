32bca16e40935bbfdcafc1622a7c495a2109494e
fauzymadani <keperluansekolahfauzy@gmail.com>
2025-02-03 19:32:32 +0700

    remove nvim

--- nvim-nvchad/lua/plugins/alpha.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "goolord/alpha-nvim",
-    config = function()
---      require("custom.configs.alpha")  -- Pastikan path ini sesuai dengan struktur direktori kamu
-    end
-  }
-}
-
